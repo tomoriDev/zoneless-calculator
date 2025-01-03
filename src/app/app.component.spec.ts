abf4f0c9a377dee6a998e13befc1288f058b668a
tomoriDev <carlostm320@gmail.com>
2025-01-03 18:15:29 -0500

    test: add first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('zoneless-calculator');
+  it('should render router-outlet wrapped with CSS classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses = 'min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5';
+
+    expect(divElement).not.toBeNull();
+    divElement?.classList.forEach(className => {
+      expect(mustHaveClasses.split(" ")).toContain(className);
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should contain the "buy me a beer" text', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+    const beerLink = compiled.querySelector('a[title="Buy me a beer"]');
+
+    console.log(beerLink)
+    
+    expect(beerLink).not.toBeNull();
+    expect(beerLink?.getAttribute('title')).toBe('Buy me a beer');
   });
 });
